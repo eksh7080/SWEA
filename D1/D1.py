d66537834e162f5a29ea43b53a4134435389cba0
eksh7080 <eksh7080@naver.com>
2022-07-04 22:33:08 +0900

    first commit

---
+++ D1/D1.py
@@ -0,0 +1,42 @@
+# 1545
+# n = int(input())
+# for i in range(n, 0-1, -1):
+#     print(i)
+
+# 2019
+# n = int(input())
+# sum = 1
+# print(1, end=" ")
+# for i in range(1, n+1):
+    
+#     sum *= 2
+
+#     print(sum, end=" ")
+
+# n = int(input())
+
+# for i in range(0, n+1):
+    
+#     i = 2**i
+
+#     print(i, end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
