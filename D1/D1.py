71b5f4faa6bddf3f80ae2058f8ab6f9b076c6763
eksh7080 <eksh7080@naver.com>
2022-07-05 21:12:51 +0900

    D1 solve

--- D1/D1.py
+++ D1/D1.py
@@ -21,16 +21,45 @@
 
 #     print(i, end=" ")
 
+# 1936
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+
+# if a == 1 and b == 3:
+#     print("a")
+# elif b == 1 and a == 3:
+#     print("b")
+# elif a < b:
+#     print("b")
+# elif a > b:
+#     print("a")
+
+# 1933
+# yak = int(input())
+
+# for i in range(1, yak+1):
+#     if yak % i == 0:
+#         print(i, end=" ")
 
 
+# 1938
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
 
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print("%d"%float(a/b))
 
+# 2025
+# n = int(input())
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
 
-
-
-
-
-
+# print(sum)
 
 
 
